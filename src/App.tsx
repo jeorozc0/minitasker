f2498ba2215bd6ffc30c5cfa8a6ca4a6237a94bf
Julio Orozco <89562415+jeorozc0@users.noreply.github.com>
2024-10-24 12:54:09 -0600

    fetch tasks in app

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import { DndContext } from '@dnd-kit/core';
 import { useDroppable } from '@dnd-kit/core';
 import AddTaskButton from './components/add-task-button';
 import { useDarkMode } from './hooks/use-dark-mode';
+import { getTasks } from './utils/local-storage';
+import { Task } from './types';
 
 
 
 function App() {
   useDarkMode();
+  const tasks = getTasks()
 
   const { setNodeRef: setFirstDroppableRef } = useDroppable({
     id: 'droppable-1',
@@ -24,7 +27,13 @@ function App() {
         <div className="flex flex-col h-full gap-8">
           <div><AddTaskButton /></div>
           <div className="grid grid-flow-col h-full w-full gap-4 grid-cols-3">
-            <div ref={setFirstDroppableRef} className="h-full bg-yellow-50 rounded-md"></div>
+            <div ref={setFirstDroppableRef} className="flex flex-col text-black gap-2 h-full bg-yellow-50 rounded-md">
+              {tasks.map((task: Task) => {
+                return (
+                  <p>{task.id}</p>
+                )
+              })}
+            </div>
             <div ref={setSecondDroppableRef} className="h-full bg-yellow-50 rounded-md"></div>
             <div ref={setThirdDroppableRef} className="h-full bg-yellow-50 rounded-md"></div>
           </div>
